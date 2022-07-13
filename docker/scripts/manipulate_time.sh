3036c938eb3b6ab5ccc14d8204b68c069c11546d
Quoc Le <songmai108@gmail.com>
2022-07-13 03:56:17 +0000

    fixed change time

--- docker/scripts/manipulate_time.sh
+++ docker/scripts/manipulate_time.sh
@@ -6,4 +6,3 @@ export LD_PRELOAD=/usr/local/lib/faketime/libfaketime.so.1 FAKETIME_NO_CACHE=1
 export FAKETIME=$1
 
 cd /app/node/ && ./restart.sh
-LD_PRELOAD=/usr/local/lib/faketime/libfaketime.so.1 FAKETIME="-15d"
