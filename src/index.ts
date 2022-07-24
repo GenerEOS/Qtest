56eb9dd88215f3a839c2616064eb7b75cd3ac5af
Quoc Le <songmai108@gmail.com>
2022-07-24 10:13:48 +0700

    fixed npm path

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { Account } from './account';
 import { Table } from './table';
 import * as Wallet from './wallet';
 import { Contract } from './contract';
+import { expectThrow, expectAction, expectBalance } from './assertion'
 
-export { Chain, Account, Table, Wallet, Contract };
+export { Chain, Account, Table, Wallet, Contract, expectThrow, expectAction, expectBalance };
