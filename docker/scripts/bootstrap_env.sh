d7223f3a2743f10a2ca1953118e134384462a5ee
danielvo11 <48626389+danielvo11@users.noreply.github.com>
2023-04-12 07:45:18 +0700

    build docker with antelope leap v3.1.4 (#57)

--- docker/scripts/bootstrap_env.sh
+++ docker/scripts/bootstrap_env.sh
@@ -3,9 +3,9 @@
 ARCH=`uname -m`
 
 if [ "${ARCH}" = "x86_64" ]; then
-   wget https://github.com/larryk85/ENF-Binaries/releases/download/v1.0/mandel_3.0.5_amd64.deb
-   apt install ./mandel_3.0.5_amd64.deb
-   rm -rf mandel_3.0.5_amd64.deb
+   wget https://github.com/AntelopeIO/leap/releases/download/v3.1.4/leap-3.1.4-ubuntu20.04-x86_64.deb
+   apt install ./leap-3.1.4-ubuntu20.04-x86_64.deb
+   rm -rf leap-3.1.4-ubuntu20.04-x86_64.deb
 else
    wget https://github.com/larryk85/ENF-Binaries/releases/download/v1.0/mandel_3.0.5_arm64.deb
    apt install ./mandel_3.0.5_arm64.deb
--- docker/scripts/deploy_system_contract_tlos.sh
+++ docker/scripts/deploy_system_contract_tlos.sh
@@ -21,12 +21,20 @@ cleos set contract eosio.msig contracts/eosio.msig eosio.msig.wasm eosio.msig.ab
 curl -X POST http://127.0.0.1:8888/v1/producer/schedule_protocol_feature_activations -d '{"protocol_features_to_activate": ["0ec7e080177b2c02b278d5088611686b49d739925a92d9bfcacd7fc6b74053bd"]}'
 sleep 2
 
-# Deploy system contracts
-until cleos set contract eosio contracts/telos eosio.system.wasm eosio.system.abi
-do
-  sleep 1s
-done
+# install eosio.boot which supports the native actions and activate 
+# action that allows activating desired protocol features prior to 
+# deploying a system contract with more features such as eosio.bios 
+# or eosio.system
+cleos set contract eosio contracts/telos eosio.system_1.7.x.wasm eosio.system_1.7.x.abi
 
+# BLOCKCHAIN_PARAMETERS
+cleos push action eosio activate '["5443fcf88330c586bc0e5f3dee10e7f63c76c00249c87fe4fbf7f38c082006b4"]' -p eosio@active
+# CONFIGURABLE_WASM_LIMITS2
+cleos push action eosio activate '["d528b9f6e9693f45ed277af93474fd473ce7d831dae2180cca35d907bd10cb40"]' -p eosio@active
+# WTMSIG_BLOCK_SIGNATURES
+cleos push action eosio activate '["299dcb6af692324b899b39f16d5a530a33062804e41f09dc97e9f156b4476707"]' -p eosio@active
+# ACTION_RETURN_VALUE
+cleos push action eosio activate '["c3a6138c5061cf291310887c0b5c71fcaffeab90d5deb50d3b9e687cead45071"]' -p eosio@active
 # GET_SENDER
 cleos push action eosio activate '["f0af56d2c5a48d60a4a5b5c903edfb7db3a736a94ed589d0b797df33ff9d3e1d"]' -p eosio@active
 # FORWARD_SETCODE
@@ -41,13 +49,26 @@ cleos push action eosio activate '["68dcaa34c0517d19666e6b33add67351d8c5f69e999c
 cleos push action eosio activate '["e0fb64b1085cc5538970158d05a009c24e276fb94e1a0bf6a528b48fbc4ff526"]' -p eosio@active
 # REPLACE_DEFERRED
 cleos push action eosio activate '["ef43112c6543b88db2283a2e077278c315ae2c84719a8b25f25cc88565fbea99"]' -p eosio@active
-# NO_DUPLIWAXE_DEFERRED_ID
+# NO_DUPLICATE_DEFERRED_ID 
 cleos push action eosio activate '["4a90c00d55454dc5b059055ca213579c6ea856967712a56017487886a4d4cc0f"]' -p eosio@active
 # ONLY_LINK_TO_EXISTING_PERMISSION
 cleos push action eosio activate '["1a99a59d87e06e09ec5b028a9cbb7749b4a5ad8819004365d02dc4379a8b7241"]' -p eosio@active
 # RAM_RESTRICTIONS
 cleos push action eosio activate '["4e7bf348da00a945489b2a681749eb56f5de00b900014e137ddae39f48f69d67"]' -p eosio@active
-sleep 1
+# GET_BLOCK_NUM
+cleos push action eosio activate '["35c2186cc36f7bb4aeaf4487b36e57039ccf45a9136aa856a5d569ecca55ef2b"]' -p eosio@active
+# WEBAUTHN_KEY
+cleos push action eosio activate '["4fca8bd82bbd181e714e283f83e1b45d95ca5af40fb89ad3977b653c448f78c2"]' -p eosio@active
+# GET_CODE_HASH
+cleos push action eosio activate '["bcd2a26394b36614fd4894241d3c451ab0f6fd110958c3423073621a70826e99"]' -p eosio@active
+# CRYPTO_PRIMITIVES
+cleos push action eosio activate '["6bcb40a24e49c26d0a60513b6aeb8551d264e4717f306b81a37a5afb3b47cedc"]' -p eosio@active
+
+# Deploy system contracts
+until cleos set contract eosio contracts/telos eosio.system.wasm eosio.system.abi
+do
+  sleep 1s
+done
 
 # Designate eosio.msig as privileged account
 cleos push action eosio setpriv '["eosio.msig", 1]' -p eosio@active
